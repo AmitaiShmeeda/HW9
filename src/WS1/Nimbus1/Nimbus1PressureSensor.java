93223529dc25993b169c0a290f01ba64c3dc5735
Amitai Shmeeda <amitaishmeeda@gmail.com>
2023-06-25 16:51:25 +0300

    Nimbus1 package

--- src/WS1/Nimbus1/Nimbus1PressureSensor.java
+++ src/WS1/Nimbus1/Nimbus1PressureSensor.java
@@ -1,4 +1,20 @@
 package WS1.Nimbus1;
 
-public class Nimbus1PressureSensor {
+public class Nimbus1PressureSensor extends Sensor {
+    private Random random;
+
+    public PressureSensor(int interval) {
+        super(interval);
+        random = new Random();
+    }
+
+    @Override
+    protected int read() {
+        return random.nextInt(101) + 950;
+    }
+
+    @Override
+    public String getName() {
+        return "pressure";
+    }
 }
--- src/WS1/Nimbus1/Nimbus1TemepratureSensor.java
+++ src/WS1/Nimbus1/Nimbus1TemepratureSensor.java
@@ -1,4 +1,20 @@
 package WS1.Nimbus1;
 
-public class Nimbus1TemepratureSensor {
+public class Nimbus1TemepratureSensor extends Sensor{
+    private Random random;
+
+    public TemperatureSensor(int interval) {
+        super(interval);
+        random = new Random();
+    }
+
+    @Override
+    protected int read() {
+        return random.nextInt(41);
+    }
+
+    @Override
+    public String getName() {
+        return "temperature";
+    }
 }
